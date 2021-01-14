5aba33c57114ef22d0c6ebc19e18a5766d4ed701
Roman Romanov <nicodim@mail.ru>
2021-01-14 13:09:16 +0300

    dev

--- config.php
+++ config.php
@@ -2,3 +2,13 @@
 if (class_exists("\Sale\Payment")) {
     \Sale\Payment::addGateway('\SalePaymentSber\Gateway');
 }
+
+if (isset($_GET['orderId'])) {
+    
+    try {
+        $order = \Sale\Order::getByTransaction($_GET['orderId']);
+        $gateway = $order->getPaymentGateway();
+        $res = $gateway->checkStatus($_GET['orderId']);      
+    }
+    catch(\Exception $e) {}
+}
