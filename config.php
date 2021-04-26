5605b292a1868e55717f70a65d55538aa802fa41
Roman Romanov <nicodim@mail.ru>
2021-04-26 16:15:17 +0300

    bugfix

--- config.php
+++ config.php
@@ -8,18 +8,6 @@ if (class_exists("\Sale\Payment")) {
     \Sale\Payment::addGateway('\SalePaymentSber\Gateway');
 }
 
-if (isset($_GET['orderId'])) {
-    
-    try {
-        $order = \Sale\Order::getByTransaction($_GET['orderId']);
-        $gateway = $order->getPaymentGateway();
-        if ($gateway) {
-            $res = $gateway->checkStatus($_GET['orderId']);  
-        }        
-    }
-    catch(\Exception $e) {}
-}
-
 if ($this->getBo()) {
 
     $this->getBo()->addEditor(array(
